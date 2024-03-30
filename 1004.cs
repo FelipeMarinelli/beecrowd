ac17f0b1d3d6d763c3302d371d895c5822074b0e
FelipeMarinelli <98427995+FelipeMarinelli@users.noreply.github.com>
2024-03-29 22:04:30 -0300

    Add files via upload

---
+++ 1004.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int a = Int32.Parse(Console.ReadLine());
+        
+        int b = Int32.Parse(Console.ReadLine());
+        
+        int Prod = a * b;
+        
+        Console.WriteLine($"PROD = {Prod}\n");
+
+    }
+
+}
